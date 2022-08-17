886bbfdc46c652aa276e4ed6d84ae159693d46ae
VIGAROME99 <89315957+VIGAROME99@users.noreply.github.com>
2022-08-17 16:44:49 -0500

    JUnit
    Creacion de Test

---
+++ CalculatorSMPBase/test/calculatorsmp/OperationsTest.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package calculatorsmp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author VICTOR
+ */
+public class OperationsTest {
+    
+    public OperationsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of MakeFormula method, of class Operations.
+     */
+    @Test
+    public void testMakeFormula() {
+        System.out.println("MakeFormula");
+        String expResult = "";
+        String result = Operations.MakeFormula();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of Solve method, of class Operations.
+     */
+    @Test
+    public void testSolve() {
+        System.out.println("Solve");
+        String formula = "";
+        String expResult = "";
+        String result = Operations.Solve(formula);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
